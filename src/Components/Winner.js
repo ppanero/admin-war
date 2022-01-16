082acb503356297596825ec06f1733d9e38adb68
Pablo Panero <pablo.panero@cern.ch>
2022-01-16 12:13:22 +0100

    winner: show as a modal

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Modal } from 'react-bootstrap';
+import { imageLoader } from '../utils';
 
-export default function Winner({ img, imgCrown }) {
+export default function Winner({ show, winner }) {
   return (
-    <div className="text-center winner-container">
-      <div className="animate__animated animate__backInDown">
-        <img className="crown" src={imgCrown} alt="Winner avatar" />
-      </div>
-      <div className="animate__animated animate__jackInTheBox">
-        <img className="winner-img" src={img} alt="Winner avatar" />
-      </div>
-    </div>
+    <Modal
+      show={show}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      {/* need to avoid imageLoader failure */}
+      {show && (
+        <Modal.Body className="war-bg">
+          <div className="text-center winner-container">
+            <div className="animate__animated animate__backInDown">
+              <img
+                className="crown"
+                src={imageLoader(`./crown.png`).default}
+                alt="Winner avatar"
+              />
+            </div>
+            <div className="animate__animated animate__jackInTheBox">
+              <img
+                className="winner-img"
+                src={imageLoader(`./${winner}.jpeg`).default}
+                alt="Winner avatar"
+              />
+            </div>
+          </div>{' '}
+        </Modal.Body>
+      )}
+    </Modal>
   );
 }
 
 Winner.propTypes = {
-  img: PropTypes.string.isRequired,
-  imgCrown: PropTypes.string.isRequired,
+  show: PropTypes.bool.isRequired,
+  winner: PropTypes.string.isRequired,
 };
