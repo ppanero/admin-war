a280fa29d0ba1e2a3874401a03d591cd76546864
Pablo Panero <pablo.panero@cern.ch>
2022-01-17 17:19:06 +0100

    war modal: add interval input

--- src/Components/WarModal.js
+++ src/Components/WarModal.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 export default function WarModal({ show, onClick }) {
+  const interval = createRef();
+
   return (
     <Modal
       show={show}
@@ -12,14 +14,20 @@ export default function WarModal({ show, onClick }) {
     >
       <Modal.Body>
         <div className="text-center">
-          <Button
-            className="war-button"
-            variant="primary"
-            size="lg"
-            onClick={onClick}
-          >
-            War!
-          </Button>
+          <Form>
+            <Form.Group className="mb-3" controlId="formInterval">
+              <Form.Label>Invervalo entre batallas (en minutos)</Form.Label>
+              <Form.Control type="textarea" placeholder="5" ref={interval} />
+            </Form.Group>
+            <Button
+              className="war-button"
+              variant="primary"
+              size="lg"
+              onClick={onClick(interval)}
+            >
+              War!
+            </Button>
+          </Form>
         </div>
       </Modal.Body>
     </Modal>
